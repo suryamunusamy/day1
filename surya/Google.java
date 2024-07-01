727d1a157dafa245dd0ba3ad3a716b691073a36f
suryamunusamy <suryaselva25@gmail.com>
2024-07-01 18:29:24 +0530

    Add files via upload
    day1

---
+++ surya/Google.java
@@ -0,0 +1,10 @@
+package surya;
+
+public class Google {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ surya/day1.java
@@ -0,0 +1,33 @@
+package surya;
+
+import java.io.StringWriter;
+
+public class day1 {
+
+	private static StringWriter Google;
+
+	public static void main(String[] args) {
+		int ticketPrize;
+		ticketPrize=100;
+		boolean Paid=true;
+		char c= 'H' ;
+		byte r=120;
+		short h=5555;
+		long j=2^4;
+		String developerType= "Google";	
+		float f=120.4f;
+				System.out.println(ticketPrize);
+				System.out.println(Paid);
+				System.out.println(c);
+				System.out.println(r);
+				System.out.println(h);
+				System.out.println(j);
+				System.out.println(f);		
+				System.out.println(developerType);
+
+
+
+	}
+}
+
+
